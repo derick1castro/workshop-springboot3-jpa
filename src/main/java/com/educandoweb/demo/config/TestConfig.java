bc3d6ce3c0fe56b7b45d340651c73e31551c5e3b
Derick <derick1castro@gmail.com>
2023-08-24 15:47:46 -0300

    Category

--- src/main/java/com/educandoweb/demo/config/TestConfig.java
+++ src/main/java/com/educandoweb/demo/config/TestConfig.java
@@ -7,9 +7,11 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.annotation.Configuration;
 
+import com.educandoweb.demo.entities.Category;
 import com.educandoweb.demo.entities.Order;
 import com.educandoweb.demo.entities.User;
 import com.educandoweb.demo.entities.enums.OrderStatus;
+import com.educandoweb.demo.repositories.CategoryRepository;
 import com.educandoweb.demo.repositories.OrderRepository;
 import com.educandoweb.demo.repositories.UserRepository;
 
@@ -22,9 +24,18 @@ public class TestConfig implements CommandLineRunner {
 	@Autowired
 	private OrderRepository orderRepository;
 
+	@Autowired
+	private CategoryRepository categoryRepository;
+	
 	@Override
 	public void run(String... args) throws Exception {
 		
+		Category cat1 = new Category(null, "Electronics");
+		Category cat2 = new Category(null, "Books");
+		Category cat3 = new Category(null, "Computers"); 
+
+		categoryRepository.saveAll(Arrays.asList(cat1, cat2, cat3));
+
 		User u1 = new User(null, "Maria Brown", "maria@gmail.com", "988888888", "123456");
 		User u2 = new User(null, "Alex Green", "alex@gmail.com", "977777777", "123456"); 
 
